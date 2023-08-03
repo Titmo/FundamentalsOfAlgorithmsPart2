02b8876a791ee8d28377acb721888e658bccd0de
03den <03denchik03@gmail.com>
2023-08-04 02:39:15 +0300

    тесты доделать

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringListImpl stringList = new StringListImpl(1000);
+        for (int i = 0; i < 1 ; i++) {
+            stringList.add(3);
+            stringList.add(1);
+            stringList.add(7);
+            stringList.add(2);
+            stringList.add(5);
+            stringList.add(7);
+            stringList.add(7);
+            stringList.add(8);
+        }
+
+        System.out.println("stringList.lastIndexOf(1) = " + stringList.indexOf(1));
+        System.out.println("stringList.toArray() = " + Arrays.toString(stringList.toArray()));
+        long start = System.currentTimeMillis();
+        stringList.sort();
+
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("stringList.toArray() = " + Arrays.toString(stringList.toArray()));
+        System.out.println("stringList.containsBin(5) = " + stringList.containsBin(8));
     }
 }
---
+++ src/test/java/org/example/StringListImplTest.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringListImplTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void containsBin() {
+    }
+}
